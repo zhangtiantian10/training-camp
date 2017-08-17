1b849331325d19f602024fcc8f442790e13f6906
yhl221 <1727538058@qq.com>
2017-08-17 19:07:54 +0800

    reducers can get the action

--- public/js/reducers/hello.js
+++
@@ -1,8 +0,0 @@
-module.exports = (state= {content: ""} , action) => {
-    if(action.type === "GET_CONTENT") {
-        state.content = action.data;
-        return Object.assign({}, state);
-    } else {
-        return state;
-    }
-}
--- public/js/reducers/index.js
+++ public/js/reducers/index.js
@@ -1,5 +1,5 @@
 import {combineReducers} from 'redux';
-import hello from './hello'
+import student from './student-reducer'
 export default combineReducers({
-    hello
+    student
 });
