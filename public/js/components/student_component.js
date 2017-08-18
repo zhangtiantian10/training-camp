d14a3dec7736ec4fd307873b3c1423a05a9d5072
yhl221 <1727538058@qq.com>
2017-08-18 15:42:37 +0800

    finish add and display

--- public/js/components/student_component.js
+++ public/js/components/student_component.js
@@ -1,75 +1,152 @@
-import React from 'react';
+import React, {Component} from "react";
 
-export default class Hello extends React.Component {
-    onSubmit(){
-        let name=document.getElementById("name").value;
-        let zone=document.getElementById("zone").value;
-        let group=document.getElementById("group").value;
-        let city=document.getElementById("city").value;
-        let school=document.getElementById("school").value;
-        let major=document.getElementById("major").value;
-        let grade=document.getElementById("grade").value;
-        let gender=document.getElementById("gender").value;
-        if(name == "" || zone == "" || group == "" || city == "" || school == "" ||major =="" || grade== "" || gender == ""){
-            alert("请补全信息");
-        }else{
-            this.props.onSubmit({name,zone,group,city,school,major,grade,gender});
-        }
-    }
+class Student extends Component {
+
 
     componentDidUpdate() {
         var isSaved = this.props.isSaved;
-        console.log("allUser",this.props.allStudent);
         if (isSaved) {
-            alert("添加成功");
-        } else {
-            alert("添加失败");
+            this.props.getAllStudent();
         }
     }
 
-    componentWillMount(){
+    componentWillMount() {
         this.props.getAllStudent();
+
     }
 
+    onSubmit() {
+        console.log("lalallaal");
+        let name = document.getElementById("name").value;
+        let zone = document.getElementById("zone").value;
+        let group = document.getElementById("team").value;
+        let city = document.getElementById("city").value;
+        let school = document.getElementById("school").value;
+        let major = document.getElementById("major").value;
+        let grade = document.getElementById("grade").value;
+        let gender = document.getElementById("gender").value;
+        if (name == "" || zone == "" || group == "" || city == "" || school == "" || major == "" || grade == "" || gender == "") {
+            alert("请补全信息");
+        } else {
+            this.props.onSubmit({name, zone, group, city, school, major, grade, gender});
+        }
+        document.getElementById("name").value = "";
+        document.getElementById("zone").value = "";
+        document.getElementById("team").value = "";
+        document.getElementById("city").value = "";
+        document.getElementById("school").value = "";
+        document.getElementById("major").value = "";
+        document.getElementById("grade").value = "";
+        document.getElementById("gender").value = "";
 
-    render () {
+    }
+
+    render() {
         return <div>
-           <div>
-               <label htmlFor="name">姓名：</label>
-               <input type="text" id="name"/>
-           </div>
-            <div>
-                <label htmlFor="zone">战区</label>
-                <input type="text" id="zone"/>
+            <div className="position">
+                <form className="form-inline">
+                    <div className="form-group">
+                        <div className="input-group">
+                            <input type="text" className="form-control"/>
+                        </div>
+                    </div>
+                    <button type="button" className="btn btn-info first">查找</button>
+                    <button type="button" className="btn btn-info first" data-toggle="modal" data-target="#firstModal">
+                        添加
+                    </button>
+                </form>
             </div>
-            <div>
-                <label htmlFor="group">组号：</label>
-                <input type="text" id="group"/>
+            <div className="tablePosition">
+                <table className="table table-bordered textStyle">
+                    <tbody className="tableStyle">
+                    <tr className="active">
+                        <th className="textStyle">姓名</th>
+                        <th className="textStyle">战区</th>
+                        <th className="textStyle">组号</th>
+                        <th className="textStyle">城市</th>
+                        <th className="textStyle">学校</th>
+                        <th className="textStyle">专业</th>
+                        <th className="textStyle">年级</th>
+                        <th className="textStyle">性别</th>
+                        <th className="textStyle" colSpan="2">操作</th>
+                    </tr>
+                    {
+                        this.props.allStudent.map((element)=> {
+                            return <tr>
+                                <td className="textStyle">{element.name}</td>
+                                <td className="textStyle">{element.zone}</td>
+                                <td className="textStyle">{element.team}</td>
+                                <td className="textStyle">{element.city}</td>
+                                <td className="textStyle">{element.school}</td>
+                                <td className="textStyle">{element.major}</td>
+                                <td className="textStyle">{element.grade}</td>
+                                <td className="textStyle">{element.gender}</td>
+                                <td><input type="button" value="修改"
+                                           className="btn btn-default"/></td>
+                                <td>
+                                    <button type="button" className="btn btn-default">
+                                        删除
+                                    </button>
+                                </td>
+                            </tr>
+                        })
+                    }
+                    </tbody>
+                </table>
             </div>
-            <div>
-                <label htmlFor="city">城市</label>
-                <input type="text" id="city"/>
-            </div>
-            <div>
-                <label htmlFor="school">学校：</label>
-                <input type="text" id="school"/>
-            </div>
-
-            <div>
-                <label htmlFor="major">专业：</label>
-                <input type="text" id="major"/>
-            </div>
-            <div>
-                <label htmlFor="grade">年级:</label>
-                <input type="text" id="grade"/>
-            </div>
-            <div>
-                <label htmlFor="gender">性别:</label>
-                <input type="text" id="gender"/>
-            </div>
-            <div>
-                <button onClick={this.onSubmit.bind(this)}>提交信息</button>
+            <div className="modal fade" id="firstModal" tabIndex="-1" role="dialog">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h4 className="modal-title position" id="exampleModalLabel">添加学生信息</h4>
+                        </div>
+                        <div className="modal-body">
+                            <form className="form-inline">
+                                <div className="inputPosition">
+                                    <label>姓名：</label>
+                                    <input type="text" className="btn btn-default" id="name"/>
+                                </div>
+                                <div className="inputPosition">
+                                    <label>战区：</label>
+                                    <input type="text" className="btn btn-default" id="zone"/>
+                                </div>
+                                <div className="inputPosition">
+                                    <label>组号：</label>
+                                    <input type="text" className="btn btn-default" id="team"/>
+                                </div>
+                                <div className="inputPosition">
+                                    <label>城市：</label>
+                                    <input type="text" className="btn btn-default" id="city"/>
+                                </div>
+                                <div className="inputPosition">
+                                    <label>学校：</label>
+                                    <input type="text" className="btn btn-default" id="school"/>
+                                </div>
+                                <div className="inputPosition">
+                                    <label>专业：</label>
+                                    <input type="text" className="btn btn-default" id="major"/>
+                                </div>
+                                <div className="inputPosition">
+                                    <label>年级：</label>
+                                    <input type="text" className="btn btn-default" id="grade"/>
+                                </div>
+                                <div className="inputPosition">
+                                    <label>性别：</label>
+                                    <input type="text" className="btn btn-default" id="gender"/>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-default" data-dismiss="modal">返回</button>
+                            <button type="button" className="btn btn-info " id="submit"
+                                    onClick={this.onSubmit.bind(this)} data-dismiss="modal">提交
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     }
 }
+
+export default Student;
