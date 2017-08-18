212eca7b575f62451c6194a35017ca972282d942
yhl221 <1727538058@qq.com>
2017-08-18 17:24:04 +0800

    start modify

--- public/js/components/student.js
+++ public/js/components/student.js
@@ -57,6 +57,43 @@ class Student extends Component {
         this.props.onSearch(student_id);
     }
 
+    insertValue(information){
+        console.log(information);
+        document.getElementById("modify_name").value = information.name;
+        document.getElementById("modify_zone").value = information.zone;
+        document.getElementById("modify_team").value = information.team;
+        document.getElementById("modify_city").value = information.city;
+        document.getElementById("modify_school").value = information.school;
+        document.getElementById("modify_major").value = information.major;
+        document.getElementById("modify_grade").value = information.grade;
+        document.getElementById("modify_gender").value = information.gender;
+    }
+
+    onModify(){
+        let id= document.getElementById("modify_id").value;
+        let name = document.getElementById("modify_name").value;
+        let zone = document.getElementById("modify_zone").value;
+        let group = document.getElementById("modify_team").value;
+        let city = document.getElementById("modify_city").value;
+        let school = document.getElementById("modify_school").value;
+        let major = document.getElementById("modify_major").value;
+        let grade = document.getElementById("modify_grade").value;
+        let gender = document.getElementById("modify_gender").value;
+        if (name == "" || zone == "" || group == "" || city == "" || school == "" || major == "" || grade == "" || gender == "") {
+            alert("请补全信息");
+        } else {
+            this.props.onModify({id,name, zone, group, city, school, major, grade, gender});
+        }
+        document.getElementById("name").value = "";
+        document.getElementById("zone").value = "";
+        document.getElementById("team").value = "";
+        document.getElementById("city").value = "";
+        document.getElementById("school").value = "";
+        document.getElementById("major").value = "";
+        document.getElementById("grade").value = "";
+        document.getElementById("gender").value = "";
+    }
+
     render() {
         return <div>
             <Nav/>
@@ -100,8 +137,8 @@ class Student extends Component {
                                 <td className="textStyle">{element.major}</td>
                                 <td className="textStyle">{element.grade}</td>
                                 <td className="textStyle">{element.gender}</td>
-                                <td><input type="button" value="修改"
-                                           className="btn btn-default"/></td>
+                                <td><button type="button" className="btn btn-default"
+                                            data-toggle="modal" data-target="#secondModal" onClick={this.insertValue.bind(this,element)}>修改</button></td>
                                 <td>
                                     <button type="button" className="btn btn-default" onClick={this.onRemove.bind(this,element.id)}>
                                         删除
@@ -159,6 +196,61 @@ class Student extends Component {
                             <button type="button" className="btn btn-default" data-dismiss="modal">返回</button>
                             <button type="button" className="btn btn-info " id="submit"
                                     onClick={this.onSubmit.bind(this)} data-dismiss="modal">提交
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="modal fade" id="secondModal" tabIndex="0" role="dialog">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h4 className="modal-title position" id="exampleModalLabel">修改学生信息</h4>
+                        </div>
+                        <div className="modal-body">
+                            <form className="form-inline">
+                                <div className="inputPosition">
+                                    <label>I D：</label>
+                                    <input type="text" className="btn btn-default" id="modify_id"/>
+                                </div>
+                                <div className="inputPosition">
+                                    <label>姓名：</label>
+                                    <input type="text" className="btn btn-default" id="modify_name"/>
+                                </div>
+                                <div className="inputPosition">
+                                    <label>战区：</label>
+                                    <input type="text" className="btn btn-default" id="modify_zone"/>
+                                </div>
+                                <div className="inputPosition">
+                                    <label>组号：</label>
+                                    <input type="text" className="btn btn-default" id="modify_team"/>
+                                </div>
+                                <div className="inputPosition">
+                                    <label>城市：</label>
+                                    <input type="text" className="btn btn-default" id="modify_city"/>
+                                </div>
+                                <div className="inputPosition">
+                                    <label>学校：</label>
+                                    <input type="text" className="btn btn-default" id="modify_school"/>
+                                </div>
+                                <div className="inputPosition">
+                                    <label>专业：</label>
+                                    <input type="text" className="btn btn-default" id="modify_major"/>
+                                </div>
+                                <div className="inputPosition">
+                                    <label>年级：</label>
+                                    <input type="text" className="btn btn-default" id="modify_grade"/>
+                                </div>
+                                <div className="inputPosition">
+                                    <label>性别：</label>
+                                    <input type="text" className="btn btn-default" id="modify_gender"/>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-default" data-dismiss="modal">返回</button>
+                            <button type="button" className="btn btn-info" onClick={this.onModify.bind(this)}
+                                    data-dismiss="modal">提交
                             </button>
                         </div>
                     </div>
