a659684e028ea4088dd9c848c1492c02a83bfb60
yhl221 <1727538058@qq.com>
2017-08-18 16:12:40 +0800

    transform state to reducer

--- public/js/reducers/student-reducer.js
+++ public/js/reducers/student-reducer.js
@@ -1,9 +1,12 @@
-module.exports = (state= {isSaved: false,allStudent:[]} , action) => {
+module.exports = (state= {isSaved: false,allStudent:[],isRemoved:false} , action) => {
     if(action.type === "ADD_STUDENT") {
         state.isSaved = action.data;
         return Object.assign({}, state);
     }else if(action.type === "GETALL_STUDENT"){
         state.allStudent=action.data;
+        return Object.assign({}, state);
+    }else if(action.type === "REMOVE_STUDENT"){
+        state.isRemoved=action.isRemoved;
         return Object.assign({}, state);
     } else {
         return state;
