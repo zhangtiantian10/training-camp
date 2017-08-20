8acfc959995a4351faf20614b4b63d84b1b99bd5
yhl221 <1727538058@qq.com>
2017-08-20 13:49:19 +0800

    add taskScore

---
+++ public/js/components/task-Score.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class TaskScore extends Component{
+    render(){
+        return <div>
+            <h1>任务卡成绩</h1>
+        </div>
+    }
+}
+
+export default TaskScore;
---
+++ public/js/containers/task-Score.js
@@ -0,0 +1,4 @@
+import {connect} from "react-redux";
+import TaskScore from "../components/task-Score";
+
+export default connect()(TaskScore);
