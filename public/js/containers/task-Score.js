585948418c7c979557f592d2caa241a7d9789621
yhl221 <1727538058@qq.com>
2017-08-20 19:47:47 +0800

    container can get the selected

--- public/js/containers/task-Score.js
+++ public/js/containers/task-Score.js
@@ -1,4 +1,20 @@
 import {connect} from "react-redux";
 import TaskScore from "../components/task-Score";
 
-export default connect()(TaskScore);
+
+const mapStateToProps = (state)=> {
+
+    return {
+
+    }
+};
+
+const mapDispatchToProps=(dispatch)=>{
+    return {
+        selectData: (data)=> {
+            dispatch({type: "SELECT_DATA", data});
+        }
+    }
+};
+
+export default connect(mapStateToProps,mapDispatchToProps)(TaskScore);
