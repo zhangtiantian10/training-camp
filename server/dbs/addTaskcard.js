c905064e25734c0109458bb2969a4a575f0b8a0e
liyanyan19940914 <921258351@qq.com>
2017-08-21 18:15:04 +0800

    insert tasks_info

--- server/dbs/addTaskcard.js
+++ server/dbs/addTaskcard.js
@@ -1,15 +1,56 @@
 function addTaskcard(res,data){
-    var connect = require('./connection');
+    var connection = require('./connection');
 
     const sql = `insert tasks_card (name) values ('${data.name}')`;
-    connect.query(sql,(err,result) => {
-        if(err){
-            console.log(err);
+    connection.beginTransaction((err) => {
+        if (err) {
             res.json(false);
-        }else{
-            res.json(true);
+            return ;
         }
-    })
+        connection.query(sql, (err, result) => {
+            if(err) {
+                return connection.rollback(() => {
+                    res.json(false);
+                    return ;
+                });
+            }
+
+            connection.query('select id from student', (err, studentIds) => {
+                if (err) {
+                    return connection.rollback(() => {
+                        res.json(false);
+                        return ;
+                    });
+                }
+
+                let insertTask = 'insert tasks_info (student_id, task_id) values';
+
+                insertTask += studentIds.map(s => {
+                    return ` (${s.id}, ${result.insertId})`
+                }).join(',');
+
+                connection.query(insertTask, (err, data) => {
+                    if (err) {
+                        return connection.rollback(() => {
+                            throw err;
+                            res.json(false);
+                            return ;
+                        });
+                    }
+                    connection.commit(function(err) {
+                        if (err) {
+                            return connection.rollback(function() {
+                                console.log(err);
+                                res.json(false);
+                                return;
+                            });
+                        }
+                        res.json(true);
+                    });
+                });
+            });
+        });
+    });
 }
 
 module.exports = addTaskcard;
