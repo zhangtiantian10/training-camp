7b67f9e8c293d295a00432ca54f137d0470023a7
yhl221 <1727538058@qq.com>
2017-08-23 10:36:42 +0800

    formate datatime

--- server/dbs/taskScore.js
+++ server/dbs/taskScore.js
@@ -1,7 +1,7 @@
 
 function selectAllTaskcard(res) {
   const connection=require('./connection');
-    const selectSql=` select a.id, a.student_id,a.task_id,a.finished_date,a.review_date,a.review_grade,a.upgrade_date,a.upgrade_grade, b.name as student_name,c.name as task_name
+    const selectSql=` select a.id, a.student_id,a.task_id,DATE_FORMAT(finished_date,'%Y-%m-%d') finished_date,DATE_FORMAT(a.review_date,'%Y-%m-%d') review_date,a.review_grade,DATE_FORMAT(a.upgrade_date,'%Y-%m-%d') upgrade_date,a.upgrade_grade, b.name as student_name,c.name as task_name
  from tasks_info a,student b,tasks_card c
  where a.student_id=b.id and a.task_id=c.id`;
     connection.query(selectSql,(err,result)=>{
