14e1ae4f4035d1f31d1c210b0ab3357220e633e7
yhl221 <1727538058@qq.com>
2017-08-24 09:53:27 +0800

    finish fliterTask

--- public/js/reducers/task-score.js
+++ public/js/reducers/task-score.js
@@ -5,8 +5,10 @@ module.exports = (state={allTask:[],isUpdate:false},action)=>{
      }else if(action.type === "MODIFY_TASK"){
          state.isUpdate=action.isUpdate;
          return Object.assign({}, state);
-     }
-     else{
+     }else if(action.type === "FILTER_TASK"){
+         state.allTask=action.fliterTask;
+         return Object.assign({}, state);
+     } else{
          return state;
      }
 };
